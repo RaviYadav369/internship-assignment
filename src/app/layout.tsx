67d21a981335e6d6381bf581fdd1593d6992dec0
Yash Shukla <shuklayash457@gmail.com>
2024-03-31 10:13:56 +0530

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <h2 className = "color-white">Sports</h2>
+        {children}</body>
     </html>
   );
 }
